9b0965656212691805fcf3928bc8ceaeb610b121
alec <alecp1996@outlook.com>
2020-09-13 18:27:56 -0400

    session authentication/cookies

---
+++ src/migrations/Migration20200913172053.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20200913172053 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
---
+++ src/resolvers/user.ts
@@ -0,0 +1,132 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
+import argon2 from 'argon2';
+
+@InputType()
+class UsernamePasswordInput {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+    @Query(() => User, {nullable: true })
+    async me(
+        @Ctx() { req, em }: MyContext
+    ) {
+        // You're not logged in
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
+    }
+
+
+    @Mutation(() => UserResponse)
+    async register(
+      @Arg("options") options: UsernamePasswordInput,
+      @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "username must be longer than 2 characters",
+                },
+            ],
+        };
+    }
+
+    if (options.password.length <= 3) {
+        return {
+            errors: [{
+                field: "password",
+                message: "password must be longer than 3 characters",
+            },
+        ],
+    };
+}
+
+        const hashedPassword = await argon2.hash(options.password);
+        const user = em.create(User, {
+            username: options.username, 
+            password: hashedPassword,
+         });
+        try {
+        await em.persistAndFlush(user);
+        } catch(err) {
+            //|| err.detail.includes("already exists")) {
+            // duplicate username error
+            if (err.code === "23505") { 
+            return {
+                errors: [
+                {
+                    field: "username",
+                    message: "this username has already been taken",
+                },
+            ],
+        };
+    }
+}
+return { user };
+}
+
+    @Mutation(() => UserResponse)
+    async login(
+      @Arg("options") options: UsernamePasswordInput,
+      @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                    field: "username",
+                    message: "username does not exist",
+                },
+            ],
+        };
+    }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "incorrect password",
+                    },
+                ],
+            };
+        }
+        // store user id session
+        // this will set a cookie on the user
+        // keeps them logged in
+        req.session!.userId = user.id;
+
+        return {
+          user,
+        };
+    }
+}
